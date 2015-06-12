7515337351e9ea037f8930d33bf4a88b7bfa9d45
Jan Bajena <jan.bajena@up-next.com>
2015-06-12 08:04:34 +0200

    Initial commit with simple expressions grammar

---
+++ MalformedGrammarException.java
@@ -0,0 +1,14 @@
+/**
+ * A class useful when the Grammar is not 
+ * correctly defined
+ */
+
+class MalformedGrammarException extends Exception{
+    MalformedGrammarException(){
+	super();
+    }
+
+    MalformedGrammarException(String s){
+	super(s);
+    }
+}
